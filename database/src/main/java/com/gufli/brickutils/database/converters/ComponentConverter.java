a94cdeb39ebf7ea7a42c3db5056945a8db21e988
Joris Guffens <joris.guffens@outlook.com>
2022-01-21 19:01:27 +0100

    added component converter

---
+++ database/src/main/java/com/gufli/brickutils/database/converters/ComponentConverter.java
@@ -0,0 +1,21 @@
+package com.gufli.brickutils.database.converters;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class ComponentConverter implements AttributeConverter<Component, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Component attribute) {
+        return GsonComponentSerializer.gson().serialize(attribute);
+    }
+
+    @Override
+    public Component convertToEntityAttribute(String dbData) {
+        return GsonComponentSerializer.gson().deserialize(dbData);
+    }
+}
